87e8062cf8be6d35c51bef2df38f3550e9197e87
Daniel-J-Mason <daniel.j.msn@gmail.com>
2024-04-05 17:03:14 -0700

    adding required fields to LocateSingleHopRouteFilter

--- trade-module/src/main/java/io/edpn/backend/trade/adapter/web/dto/filter/RestLocateSingleHopRouteFilterDto.java
+++ trade-module/src/main/java/io/edpn/backend/trade/adapter/web/dto/filter/RestLocateSingleHopRouteFilterDto.java
@@ -1,19 +1,21 @@
 package io.edpn.backend.trade.adapter.web.dto.filter;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.util.List;
 
 public record RestLocateSingleHopRouteFilterDto(
-        Double xCoordinate,
-        Double yCoordinate,
-        Double zCoordinate,
+        @NotNull Double xCoordinate,
+        @NotNull Double yCoordinate,
+        @NotNull Double zCoordinate,
         List<String> commodityDisplayNames,
-        Integer maxPriceAgeHours,
-        Integer maxRouteDistance,
-        String maxLandingPadSize,
-        Integer maxArrivalDistance,
-        Integer minSupply,
-        Integer minDemand,
-        Boolean includeSurfaceStations,
-        Boolean includeFleetCarriers) {
+        @NotNull Integer maxPriceAgeHours,
+        @NotNull Integer maxRouteDistance,
+        @NotNull String maxLandingPadSize,
+        @NotNull Integer maxArrivalDistance,
+        @NotNull Integer minSupply,
+        @NotNull Integer minDemand,
+        @NotNull Boolean includeSurfaceStations,
+        @NotNull Boolean includeFleetCarriers) {
 
 }
