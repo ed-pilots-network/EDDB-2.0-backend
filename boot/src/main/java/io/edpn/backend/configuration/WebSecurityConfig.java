4880856c3b10ae7e9e1c8c462322c7a082bd9c99
Pieter Van Eeckhout <vaneeckhout.pieter@gmail.com>
2024-05-15 07:36:56 +0200

    #176 - Update allowed origin in CORS configuration (#177)
    * #176 - Update allowed origin in CORS configuration
    
    The commit changes the allowed origin in the CORS configuration from "http" to "https" for the "edpn.io" domain. This fortifies the security of the application by ensuring communications are made over a secure connection.
    
    * add both http + https for localhost and edpn origins to unblock FE
    
    ---------
    
    Co-authored-by: Daniel-J-Mason <daniel.j.msn@gmail.com>

--- boot/src/main/java/io/edpn/backend/configuration/WebSecurityConfig.java
+++ boot/src/main/java/io/edpn/backend/configuration/WebSecurityConfig.java
@@ -24,7 +24,9 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("https://localhost:3000");
         configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedOrigin("https://*.edpn.io*");
         configuration.addAllowedOrigin("http://*.edpn.io*");
         configuration.addAllowedMethod(CorsConfiguration.ALL);
         configuration.addAllowedHeader(CorsConfiguration.ALL);
